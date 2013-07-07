233239c4b611d9354211c2d38da5799c00cd6766
Gama11 <jensfischer95@gmail.com>
2013-07-07 23:21:45 +0200

    Update template

--- $(BaseDir)/Projects/395 HaXe - Flixel Project/Source/$(PackagePath)/Reg.hx
+++ $(BaseDir)/Projects/395 HaXe - Flixel Project/Source/$(PackagePath)/Reg.hx
@@ -1,12 +1,12 @@
 package;
 
-import org.flixel.FlxSave;
+import flixel.util.FlxSave;
 
 /**
-* Handy, pre-built Registry class that can be used to store 
-* references to objects and other things for quick-access. Feel
-* free to simply ignore it or change it in any way you like.
-*/
+ * Handy, pre-built Registry class that can be used to store
+ * references to objects and other things for quick-access. Feel
+ * free to simply ignore it or change it in any way you like.
+ */
 class Reg
 {
 	/**
