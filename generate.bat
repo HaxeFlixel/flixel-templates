ddaf41e1304039b340a8099760195dd360f47b13
Gama11 <jensfischer95@gmail.com>
2013-12-28 09:53:26 +0100

    Add generate.bat to generate templates for all IDEs, add directory with pregenerated templates, #7

---
+++ generate.bat
@@ -0,0 +1,30 @@
+rmdir pregenerated /s /q
+
+md pregenerated
+cd pregenerated
+
+haxelib run flixel-tools tpl -fd
+cd default
+7z a -tzip ../flash-develop.zip
+cd ..
+rmdir default /s /q
+
+haxelib run flixel-tools tpl -subl
+cd default
+7z a -tzip ../sublime-text.zip
+cd..
+rmdir default /s /q
+
+haxelib run flixel-tools tpl -idea
+cd default
+7z a -tzip ../intelij-idea.zip
+cd..
+rmdir default /s /q
+
+cd ..
+cd ide-data/flash-develop-fdz
+7z a -tzip ../../FlxProject.zip
+cd ..\..\
+move /y "FlxProject.zip" "pregenerated\FlxProject.fdz" 
+
+pause
