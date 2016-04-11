37399fb12af2948aee44c6f90f029b5b2bfa58a8
buckle2000 <buckle2000@users.noreply.github.com>
2016-04-11 16:28:20 +0800

    Update Main.hx

--- default/source/Main.hx
+++ default/source/Main.hx
@@ -9,6 +9,6 @@ class Main extends Sprite
     public function new()
     {
         super();
-        addChild(new FlxGame(Std.int(Lib.current.stage.width), Std.int(Lib.current.stage.height), MenuState));
+        addChild(new FlxGame(0, 0, MenuState));
     }
 }
