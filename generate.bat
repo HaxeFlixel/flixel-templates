78d2c8535b26e4faa19cb2de062b29f9067dd5e0
Cameron Taylor <cameron.taylor.ninja@gmail.com>
2025-03-27 09:54:00 -0400

    remove all Flash Develop templates (#49)

--- generate.bat
+++ generate.bat
@@ -3,12 +3,6 @@ rmdir pregenerated /s /q
 md pregenerated
 cd pregenerated
 
-haxelib run flixel-tools tpl -ide fd
-cd default
-7z a -tzip ../flash-develop.zip
-cd ..
-rmdir default /s /q
-
 haxelib run flixel-tools tpl -ide subl
 cd default
 7z a -tzip ../sublime-text.zip
@@ -20,11 +14,5 @@ cd default
 7z a -tzip ../intelij-idea.zip
 cd..
 rmdir default /s /q
-
-cd ..
-cd ide-data/flash-develop-fdz
-7z a -tzip ../../FlxProject.zip
-cd ..\..\
-move /y "FlxProject.zip" "pregenerated\FlxProject.fdz"
 
 pause
